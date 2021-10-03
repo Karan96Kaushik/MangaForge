796c90e392bc346035a156ef4bd4fa1a9c78bf80
Karan <karankshk96@gmail.com>
2021-10-03 08:05:56 +0530

    binary added

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express()
 const path = require('path')
 const fs = require('fs');
 const cors = require('cors')
+const { spawn } = require('child_process');
 
 global.path = require('path');
 
@@ -18,43 +19,64 @@ app.set('etag', false);
 
 console.log('Server started')
 
+app.post('/download', async (req, res) => {
+
+	let url = req.body.url
+
+	const dowloader = spawn('./downloaderBin', ['--url='+url]);
+
+	dowloader.stdout.on('data'	, (data) => console.log(`stdout: ${data}`))
+	dowloader.stderr.on('data'	, (data) => console.log(`err: ${data}`))
+
+	dowloader.on('close', (code) => {
+		console.log(`exit: ${code}`)
+		res.send("Completed")
+	})
+
+})
+
+
+
+
+
+
 app.get('/files', (req, res) => {
-    var html = ` <head></head><body>`
-    var files = fs.readdirSync(__dirname + '/Comix/');
+	var html = ` <head></head><body>`
+	var files = fs.readdirSync(__dirname + '/Comix/');
 
-    files.forEach(element => {
-        var lnk = '<a href="/Comix/' + element + '">' + element + '</a><br>'
-        html += lnk
-    });
+	files.forEach(element => {
+		var lnk = '<a href="/Comix/' + element + '">' + element + '</a><br>'
+		html += lnk
+	});
 
-    html += '</body>'
-    res.send(html);
+	html += '</body>'
+	res.send(html);
 })
 
 
 app.get('/jsonfolders', (req, res) => {
-    catalogger()
-    res.json(folders);
+	catalogger()
+	res.json(folders);
 })
 
 app.get('/json', (req, res) => {
-    catalogger()
-    res.json(tree_pics[req.query.folder]);
+	catalogger()
+	res.json(tree_pics[req.query.folder]);
 })
 
 app.get('/jsonvids', (req, res) => {
-    catalogger()
-    vids = []
-    tree_vids[req.query.folder].forEach(el => {
-        vids. push({
-            title: el,
-            href: '/Comix/' + req.query.folder + '/' + el,
-            type: 'video/mp4',
-            //poster: 'https://i.imgur.com/MUSw4Zu.jpg'
-        })
-    })
+	catalogger()
+	vids = []
+	tree_vids[req.query.folder].forEach(el => {
+		vids. push({
+			title: el,
+			href: '/Comix/' + req.query.folder + '/' + el,
+			type: 'video/mp4',
+			//poster: 'https://i.imgur.com/MUSw4Zu.jpg'
+		})
+	})
    // console.log(vids);
-    res.json(vids);
+	res.json(vids);
 })
 
 var tree_vids = {}
@@ -63,37 +85,37 @@ var tree_pics = {}
 var folders = [];
 
 function catalogger() {
-    tree_vids = {}
-    tree_pics = {}
-    folders = [];
-    
-    var filesfolders = fs.readdirSync(__dirname + '/Comix/')
+	tree_vids = {}
+	tree_pics = {}
+	folders = [];
+	
+	var filesfolders = fs.readdirSync(__dirname + '/Comix/')
 
 
-    filesfolders.forEach(el => {
-        if(el.endsWith('.jpg') || el.endsWith('.html') || el.endsWith('.pdf') || el.endsWith('.png') || el.endsWith('.mp4') || el.endsWith('.ico') || el.endsWith('.sql')) {
-        } else {
-            folders.push(el)
-        }
-    })
-    
-    folders.forEach( el => {
-        tree_pics[el] = []
-        var files = fs.readdirSync(__dirname + '/Comix/' + el)
-        files.forEach(pic => {
-            if(pic.endsWith('.jpg')) {
-                tree_pics[el].push(pic)
-            }
-        })
-    })
-    
-    folders.forEach( el => {
-        tree_vids[el] = []
-        var files = fs.readdirSync(__dirname + '/Comix/' + el)
-        files.forEach(vid => {
-            if(vid.endsWith('.mp4')) {
-                tree_vids[el].push(vid)
-            }
-        })
-    })
+	filesfolders.forEach(el => {
+		if(el.endsWith('.jpg') || el.endsWith('.html') || el.endsWith('.pdf') || el.endsWith('.png') || el.endsWith('.mp4') || el.endsWith('.ico') || el.endsWith('.sql')) {
+		} else {
+			folders.push(el)
+		}
+	})
+	
+	folders.forEach( el => {
+		tree_pics[el] = []
+		var files = fs.readdirSync(__dirname + '/Comix/' + el)
+		files.forEach(pic => {
+			if(pic.endsWith('.jpg')) {
+				tree_pics[el].push(pic)
+			}
+		})
+	})
+	
+	folders.forEach( el => {
+		tree_vids[el] = []
+		var files = fs.readdirSync(__dirname + '/Comix/' + el)
+		files.forEach(vid => {
+			if(vid.endsWith('.mp4')) {
+				tree_vids[el].push(vid)
+			}
+		})
+	})
 }
