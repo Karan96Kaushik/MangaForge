5b34f8ebb1a1841aa8b23f869697eeb4da22ca39
Karan96Kaushik <karankshk96@gmail.com>
2024-06-07 19:32:56 +0100

    removed crap

--- xn.py
+++
@@ -1,9 +0,0 @@
-from xnxx import page as xnxx
-
-count = -1
-
-while(True):
-    count = count + 1
-    file1 = open('myfile.txt', 'r') 
-    Lines = file1.readlines() 
-    xnxx(Lines[count], 'XNXX')
